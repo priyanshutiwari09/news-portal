be9b90a8e1d121f34311df9ac428c05923cb75ac
priyanshutiwari09 <priyanshuganeshtiwari@gmail.com>
2025-07-10 20:56:12 +0530

    server.js edited

--- backend/server.js
+++ backend/server.js
@@ -37,10 +37,11 @@ app.use("/news", newsRoute);
 
 // ✅ Serve frontend (SPA support)
 // ✅ CORRECT: Go one level up to get the frontend folder
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
 
 
 // Start server
