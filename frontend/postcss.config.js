c46fcdb4f7d1471a68d64c1545b5ac7f9a31c2eb
priyanshutiwari09 <priyanshuganeshtiwari@gmail.com>
2025-07-13 14:45:49 +0530

    updated css

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js (ESM style if "type": "module")
-import tailwindcssPostcss from '@tailwindcss/postcss';
-
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-};
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,27 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+//   build: {
+//     outDir: 'dist',
+//     emptyOutDir: true
+//   },
+//   server: {
+//     proxy: {
+//       '/user': 'http://localhost:5000',
+//       '/news': 'http://localhost:5000'
+//     }
+//   }
+// });
+
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ very important for correct asset paths
-  build: {
-    outDir: "dist",
-    emptyOutDir: true
-  },
   server: {
     proxy: {
       "/user": "http://localhost:5000",
